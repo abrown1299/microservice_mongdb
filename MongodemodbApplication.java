36fce495675d88af130dfaa793a9678ccafd1a66
Araceli M Brown <70526594+abrown1299@users.noreply.github.com>
2020-10-18 16:47:23 -0500

    Add files via upload

---
+++ MongodemodbApplication.java
@@ -0,0 +1,15 @@
+package com.example.mongodemodb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MongodemodbApplication
+{
+
+    public static void main(String[] args)
+    {
+        SpringApplication.run(MongodemodbApplication.class, args);
+    }
+
+}
